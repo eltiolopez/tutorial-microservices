1055dbab0e08d06461795bd8d87344e7f06dbfef
jld <javier.lpz.dlg@gmail.com>
2023-10-12 22:16:03 +0200

    keycloak config fix

---
+++ gateway-service/src/main/java/com/jld/gatewayservice/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.jld.gatewayservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+	/*@Bean
+	public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+		http.authorizeExchange().anyExchange().authenticated()
+			.and()
+			.oauth2Login(Customizer.withDefaults());
+		http.csrf().disable();
+		return http.build();
+	}*/
+	
+	@Bean
+	public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+	    http
+	        .authorizeExchange(exchanges ->
+	            exchanges
+	                .anyExchange().authenticated()
+	        )
+	        .oauth2Login(Customizer.withDefaults());
+	    http.csrf(csrf -> csrf.disable());
+	    return http.build();
+	}
+}
